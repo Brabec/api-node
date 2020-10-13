4de4914dabfe3949e1f5cb202882a4dcd24f6bb2
Rafael Barbosa <rafael@infodev.com.br>
2020-10-13 16:42:13 -0300

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+// Import Routes
+const authRoutes = require('./routes/auth');
+const postRoutes = require('./routes/posts');
+
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('Connected to db!');
+  }
+);
+
+// Middleware
+app.use(express.json());
+
+// Route Middleware
+app.use('/api/user', authRoutes);
+app.use('/api/posts', postRoutes);
+
+app.listen(3000, () => console.log('Server up and running'));
